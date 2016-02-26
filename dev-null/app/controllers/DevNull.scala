e6ae4c611ea3a8e20cc9474cb53e735b770f4d35
Mark Siebert <mark@lucidchart.com>
2016-02-25 20:04:30 -0700

    dev-null project now returns failures randomly so the client will have more interesting results

--- dev-null/app/controllers/DevNull.scala
+++ dev-null/app/controllers/DevNull.scala
@@ -1,9 +1,17 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import scala.util.Random
 
 object DevNull extends Controller {
+  private[this] val rand = new Random
+  private[this] val failures = Seq(BadRequest, Forbidden, InternalServerError)
+
   def index = Action {
-    Ok
+    if (rand.nextInt(100) > 25) {
+      Ok
+    } else {
+      rand.shuffle(failures).head
+    }
   }
 }
